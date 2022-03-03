008e710b1818cbe24c402b03d6dac7e0dbf358bb
Kiki-hermawan <72023427+Kiki-hermawan@users.noreply.github.com>
2022-03-03 19:15:21 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,12 @@
+<?php
+
+ $nama  = "Kiki Hermawan"; 
+ $nim   = "20090066";  
+ $kelas = "4B";
+
+ echo "Halo! Perkenalkan Saya,\n";
+ echo "Nama     : $nama\n";
+ echo "Nim      : $nim\n";
+ echo "Kelas    : $kelas\n";
+
+?>
